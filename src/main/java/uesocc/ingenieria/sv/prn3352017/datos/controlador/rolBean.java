822157b11339e05ab1e74b03340625e6ce48808a
CM16020-prn335 <kchinchillam98@gmail.com>
2017-10-19 17:10:59 -0600

    crud

--- src/main/java/uesocc/ingenieria/sv/prn3352017/datos/controlador/rolBean.java
+++ src/main/java/uesocc/ingenieria/sv/prn3352017/datos/controlador/rolBean.java
@@ -15,7 +15,9 @@ import javax.faces.application.FacesMessage;
 import javax.faces.context.FacesContext;
 import javax.faces.view.ViewScoped;
 import javax.inject.Named;
+import org.primefaces.component.panel.Panel;
 import org.primefaces.event.SelectEvent;
+import org.primefaces.event.UnselectEvent;
 import uesocc.ingenieria.sv.prn3352017.datos.accseso.RolFacadeLocal;
 import uesocc.ingenieria.sv.prn3352017.datos.definiciones.Rol;
 
@@ -27,10 +29,14 @@ public class rolBean implements Serializable{
     @EJB 
     RolFacadeLocal RolFacade;
     Rol rol = new Rol();
-     List<Rol> listRol=new ArrayList<>();
-     boolean activo;
+    List<Rol> listRol=new ArrayList<>();
+    boolean activo;
+    Panel panel1 = new Panel();
+    Panel panel2 = new Panel();
+    
        
-       public rolBean(){}
+       public rolBean(){
+       }
 
      @PostConstruct
     public void init() {
@@ -39,14 +45,15 @@ public class rolBean implements Serializable{
        }else{
            this.listRol= Collections.EMPTY_LIST;
        }
+       panel2.setVisible(false);
     }
     
       public void crear() {
             try {
                 RolFacade.create(rol);
-                init();
+                
                // showMessage("Registro ingresado correctamente.");
-               
+                limpiar();
             } catch (Exception e) {
                 System.out.println("Error: " + e);
                 showMessage("Error al ingresar los datos.");
@@ -68,7 +75,7 @@ public class rolBean implements Serializable{
                 RolFacade.edit(rol);
                 init();
                // showMessage("Registro ingresado correctamente.");
-                rol = null;
+                
             } catch (Exception e) {
                 System.out.println("Error: " + e);
                 showMessage("Error al ingresar los datos.");
@@ -77,7 +84,11 @@ public class rolBean implements Serializable{
        }
          
       public void limpiar(){
-       rol = null;
+        if (rol!=null) {
+       rol=null;
+       panel1.setVisible(true);
+       panel2.setVisible(false);
+          }
       }
       
       public void addMessage(String summary) {
@@ -100,13 +111,16 @@ public class rolBean implements Serializable{
         
     public void onRowSelect(SelectEvent event) {
         rol = (Rol) event.getObject();
+        panel1.setVisible(false);
+        panel2.setVisible(true);
+
     }
- /*
+ 
     public void onRowUnselect(UnselectEvent event) {
-        FacesMessage msg = new FacesMessage("Car Unselected", ((Car) event.getObject()).getId());
-        FacesContext.getCurrentInstance().addMessage(null, msg);
+        
+        
     }
-*/
+
     public Rol getRol() {
         return rol;
     }
@@ -137,6 +151,22 @@ public class rolBean implements Serializable{
 
     public void setActivo(boolean activo) {
         this.activo = activo;
+    }
+
+    public Panel getPanel1() {
+        return panel1;
+    }
+
+    public void setPanel1(Panel panel1) {
+        this.panel1 = panel1;
+    }
+
+    public Panel getPanel2() {
+        return panel2;
+    }
+
+    public void setPanel2(Panel panel2) {
+        this.panel2 = panel2;
     }
 
    
